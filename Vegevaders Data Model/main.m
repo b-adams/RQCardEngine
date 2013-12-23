0940073128668c9991f50f0857ade296ab0155fb
Bryant Adams <badams.maps+ordo_git_120930@gmail.com>
2013-12-22 21:26:51 -0500

    Initialize Xcode5 project

---
+++ Vegevaders Data Model/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Vegevaders Data Model
+//
+//  Created by Bryant Adams on 12/22/13.
+//  Copyright (c) 2013 Plants vs Pathogens: Vegevaders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
